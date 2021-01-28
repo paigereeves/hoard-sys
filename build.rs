36989b877962f086267b1d21d5008c9df0fd0984
paigereeves <paigereeves006@gmail.com>
2021-01-28 15:20:52 +1100

    Build Hoard into a static library.

--- build.rs
+++ build.rs
@@ -6,24 +6,35 @@ const VERSION: &'static str = "b24588fe050a230f79666b50495f1f239aa537d9";
 fn main() {
     let mut current_dir = PathBuf::from(std::env::var("OUT_DIR").expect("No OUT_DIR."));
     if !current_dir.join("Hoard").exists() {
-        let mut git = Command::new("git");
-        git
+        let mut cmd = Command::new("git");
+        cmd
             .current_dir(&current_dir)
             .args(&["clone", "-n", "https://github.com/emeryberger/Hoard"])
             .status().expect("Failed to clone Hoard.");
     }
+
     current_dir.push("Hoard");
-    let mut git = Command::new("git");
-    git
+
+    let mut cmd = Command::new("git");
+    cmd
         .current_dir(&current_dir)
         .args(&["checkout", VERSION])
         .status().expect("Failed to checkout.");
 
     current_dir.push("src");
-    let mut make = Command::new("make");
-    make
+
+    let mut cmd = Command::new("make");
+    cmd
         .current_dir(&current_dir)
+        .arg("Heap-Layers")
+        .status().expect("Failed to clone Heap Layers.");
+
+    let mut cmd = Command::new("make");
+    cmd
+        .current_dir("./")
+        .args(&["Hoard", &format!("out={}",&current_dir.to_str().unwrap())])
         .status().expect("Failed to make Hoard.");
-    println!("cargo:rustc-link-lib=dylib=hoard");
+
+    println!("cargo:rustc-link-lib=static=hoard");
     println!("cargo:rustc-link-search=native={}", &current_dir.to_str().unwrap());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use libc::{size_t, c_void};
 
-#[link(name = "hoard")]
+#[link(name = "hoard", kind = "static")]
 extern "C" {
     pub fn calloc(n: size_t, size: size_t) -> *mut c_void;
     pub fn free(p: *mut c_void);
