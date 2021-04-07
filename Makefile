7c9dff867f0df8a2c726d08f62e4b375d7c726d0
Yi Lin <qinsoon@gmail.com>
2021-04-07 10:33:28 +1000

    Add submodules for hoard depeendency. Remove Makefile. Fix build.rs

--- Makefile
+++
@@ -1,6 +0,0 @@
-include $(out)/GNUmakefile
-
-CPPFLAGS=-std=c++14 -flto -O3 -DNDEBUG -ffast-math -fno-builtin-malloc -Wall -Wextra -Wshadow -Wconversion -Wuninitialized -Dalways_inline= 
-
-Hoard:
-	cd $(out) && $(MAKE) $(shell uname -s)-gcc-$(shell uname -p)-static CPPFLAGS="$(CPPFLAGS)"
--- build.rs
+++ build.rs
@@ -1,40 +1,60 @@
 use std::process::Command;
 use std::path::PathBuf;
 
-const VERSION: &'static str = "b24588fe050a230f79666b50495f1f239aa537d9";
+const CPPFLAGS: &'static str = "-std=c++14 -flto -O3 -DNDEBUG -ffast-math -fno-builtin-malloc -Wall -Wextra -Wshadow -Wconversion -Wuninitialized -Dalways_inline=";
 
 fn main() {
-    let mut current_dir = PathBuf::from(std::env::var("OUT_DIR").expect("No OUT_DIR."));
-    if !current_dir.join("Hoard").exists() {
-        let mut cmd = Command::new("git");
-        cmd
-            .current_dir(&current_dir)
-            .args(&["clone", "-n", "https://github.com/emeryberger/Hoard"])
-            .status().expect("Failed to clone Hoard.");
+    // target/{build}/hoard-sys-{hash}/out
+    let build_dir = PathBuf::from(std::env::var("OUT_DIR").expect("No OUT_DIR."));
+    // target/{build}/hoard-sys-{hash}/out/Hoard
+    let hoard_build = build_dir.join("Hoard");
+    // target/{build}/hoard-sys-{hash}/out/Hoard/src
+    let hoard_src = build_dir.join("Hoard/src");
+    // target/{build}/hoard-sys-{hash}/out/Hoard/src/Heap-Layers
+    let heaplayers_build = build_dir.join("Hoard/src/Heap-Layers");
+
+    // Project root and submodules
+    let this = PathBuf::from(file!()).canonicalize().unwrap();
+    let root_dir = this.parent().unwrap();
+    let hoard_module = root_dir.join("Hoard");
+    let heaplayers_module = root_dir.join("Heap-Layers");
+
+    // Copy Hoard submodule to hoard_build
+    if !hoard_build.exists() {
+        let mut cmd = Command::new("cp");
+        cmd.current_dir(&build_dir).args(&["-r", &hoard_module.to_str().unwrap(), &hoard_build.to_str().unwrap()]);
+        run_command(&mut cmd);
+        assert!(hoard_build.exists(), "Hoard is not symlinked properly: {:?}", hoard_build);
     }
 
-    current_dir.push("Hoard");
+    // Copy Heap-Layers submodule to heaplayers_build
+    if !heaplayers_build.exists() {
+        let mut cmd = Command::new("cp");
+        cmd.current_dir(&hoard_src).args(&["-r", &heaplayers_module.to_str().unwrap(), &heaplayers_build.to_str().unwrap()]);
+        run_command(&mut cmd);
+        assert!(heaplayers_build.exists(), "Heap-Layers is not symlinked properly: {:?}", heaplayers_build);
+    }
 
-    let mut cmd = Command::new("git");
-    cmd
-        .current_dir(&current_dir)
-        .args(&["checkout", VERSION])
-        .status().expect("Failed to checkout.");
-
-    current_dir.push("src");
-
+    // Run make in hoard_src
     let mut cmd = Command::new("make");
-    cmd
-        .current_dir(&current_dir)
-        .arg("Heap-Layers")
-        .status().expect("Failed to clone Heap Layers.");
-
-    let mut cmd = Command::new("make");
-    cmd
-        .current_dir("./")
-        .args(&["Hoard", &format!("out={}",&current_dir.to_str().unwrap())])
-        .status().expect("Failed to make Hoard.");
+    cmd.current_dir(&hoard_src).args(&[&get_hoard_build_string(), &format!("CPPFLAGS={}", CPPFLAGS)]);
+    run_command(&mut cmd);
 
     println!("cargo:rustc-link-lib=static=hoard");
-    println!("cargo:rustc-link-search=native={}", &current_dir.to_str().unwrap());
+    println!("cargo:rustc-link-search=native={}", &hoard_src.to_str().unwrap());
+}
+
+fn run_command(cmd: &mut Command) {
+    match cmd.status() {
+        Ok(ret) => assert!(ret.success(), "{:?} returned non-zero: {:?}", cmd, ret),
+        Err(e) => panic!("Failed to run command {:?}: {:?}", cmd, e),
+    }
+}
+
+fn get_hoard_build_string() -> String {
+    use uname::uname;
+    match uname() {
+        Ok(info) => format!("{}-gcc-{}-static", info.sysname, info.machine),
+        Err(e) => panic!("Failed to get uname: {:?}", e),
+    }
 }
