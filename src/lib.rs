f6a35f2f8adfda6f8457e5d15c3db21c42d56aef
Yi Lin <qinsoon@gmail.com>
2022-05-16 11:01:52 +1000

    Expose malloc()

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use libc::{size_t, c_void};
 
 #[link(name = "hoard", kind = "static")]
 extern "C" {
+    pub fn malloc(size: size_t) -> *mut c_void;
     pub fn calloc(n: size_t, size: size_t) -> *mut c_void;
     pub fn free(p: *mut c_void);
     pub fn malloc_usable_size(p: *mut c_void) -> size_t;
